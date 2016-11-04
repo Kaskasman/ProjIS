4b2dea84df5e60b83a01fc83d644834686fe617d
HugoDuarte <HugoDuarte@DESKTOP-DGHN76B>
2016-11-04 17:57:27 +0000

    Finalizado

--- ProjIS/FormProjIS/Form1.cs
+++ ProjIS/FormProjIS/Form1.cs
@@ -18,10 +18,8 @@ namespace FormProjIS
 {
     public partial class Form1 : Form
     {
-        private List<Exercicio> exercicios = new List<Exercicio>();
-        private String line;
-        String xml;
-        String xsd;
+        private static String xml;
+        private static String xsd;
 
         public Form1()
         {
@@ -35,43 +33,18 @@ namespace FormProjIS
 
             if (openFileDialog1.ShowDialog() == System.Windows.Forms.DialogResult.OK)
             {
-                XmlDocument doc = new XmlDocument();
-                XmlDeclaration dec = doc.CreateXmlDeclaration("1.0", null, null);
-                doc.AppendChild(dec);
-                XmlElement exerciciosXml = doc.CreateElement("exercicios");
-                doc.AppendChild(exerciciosXml);
-                using (StreamReader r = new StreamReader(openFileDialog1.FileName))
+                try
                 {
-                    string json = r.ReadToEnd();
-                    exercicios = JsonConvert.DeserializeObject<List<Exercicio>>(json);
-                    for (int i = 0; i < exercicios.Count; i++)
-                    {
-                        string linha = exercicios[i].ToString();
-                        string[] nomes = linha.Split(' ');
-                        XmlElement exercicioXml = doc.CreateElement("exercicio");
-                        XmlElement nomeXml = doc.CreateElement("nome");
-                        foreach (string s in nomes)
-                        {
-                            if (s != nomes[nomes.Length - 2] && s != nomes[nomes.Length - 1])
-                            {
-                                nomeXml.InnerText += s + " ";
-                            }
-                        }
-                        nomeXml.InnerText.Trim();
-                        XmlElement kcalXml = doc.CreateElement("kcal");
-                        kcalXml.InnerText = nomes[nomes.Length - 2];
-                        XmlElement metXml = doc.CreateElement("met");
-                        metXml.InnerText = nomes[nomes.Length - 1];
-                        exercicioXml.AppendChild(nomeXml);
-                        exercicioXml.AppendChild(kcalXml);
-                        exercicioXml.AppendChild(metXml);
-                        exerciciosXml.AppendChild(exercicioXml);
-                    }
-                    doc.Save(@"exerciciosXml.xml");
-                    tb_filePath.Text = System.IO.Directory.GetCurrentDirectory().ToString() + "\\exerciciosXml.xml";
-                    FormProjIS.Properties.Settings.Default.XML = tb_filePath.Text;
+                    String filePath = HandlerXmlXsdcs.criarXMLJson(openFileDialog1.FileName);
+                    tb_filePath.Text = filePath;
                     MessageBox.Show("XML criado com sucesso", "Sucesso", MessageBoxButtons.OK);
                 }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message, "Erro", MessageBoxButtons.OK);
+                    throw;
+                }
+                
             }
         }
 
@@ -80,80 +53,19 @@ namespace FormProjIS
             openFileDialog1.Filter = "TXT Files|*.txt";
             openFileDialog1.InitialDirectory = Application.StartupPath;
 
-            String final = "";
-
-            XmlDocument doc = new XmlDocument();
-            XmlDeclaration dec = doc.CreateXmlDeclaration("1.0", null, null);
-            doc.AppendChild(dec);
-
-            XmlElement vegetais = doc.CreateElement("vegetais");
-            doc.AppendChild(vegetais);
-
             if (openFileDialog1.ShowDialog() == System.Windows.Forms.DialogResult.OK)
             {
-                StreamReader s = new StreamReader(openFileDialog1.FileName);
-                
-
-                while ((line = s.ReadLine()) != null)
+                try
                 {
-                    string[] linha = line.Split('(', ')');
-                    string[] nomes = linha[0].Trim().Split(' ');
-                    string nome = "";
-                    string estado = "-";
-                    string calorias = "";
-                    string dose = "";
-
-                    for (int i = 4; i < nomes.Length; i++)
-                    {
-                        nome += nomes[i] + " ";
-                    }
-
-                    if (linha.Length > 3)
-                    {
-                        estado = linha[linha.Length - 4];
-                        dose += linha[linha.Length - 2];
-                        calorias += nomes[1];
-                        if (linha.Length >= 4)
-                        {
-                            nome += linha[1];
-                        }
-                    }
-                    else if (linha.Length <= 3)
-                    {
-                        dose += linha[linha.Length - 2];
-                        calorias += nomes[1];
-                    }
-
-                    XmlElement vegetal = doc.CreateElement("vegetal");
-
-                    XmlElement nomeXml = doc.CreateElement("nome");
-                    nomeXml.InnerText = nome.Trim();
-
-                    XmlElement kcalXml = doc.CreateElement("kcal");
-                    kcalXml.InnerText = calorias;
-
-                    XmlElement estadoXml = doc.CreateElement("estado");
-                    estadoXml.InnerText = estado;
-
-                    XmlElement doseXml = doc.CreateElement("dose");
-                    doseXml.InnerText = dose;
-
-                    vegetal.AppendChild(nomeXml);
-                    vegetal.AppendChild(kcalXml);
-                    vegetal.AppendChild(estadoXml);
-                    vegetal.AppendChild(doseXml);
-                    vegetais.AppendChild(vegetal);
-
-                    //para tirar futuro
-                    Vegetal veg = new Vegetal(nome, estado, calorias, dose);
-                    final += veg.ToString() + "\n";
+                    String filePath = HandlerXmlXsdcs.criarXMLTxt(openFileDialog1.FileName);
+                    tb_filePath.Text = filePath;
+                    MessageBox.Show("XML criado com sucesso", "Sucesso", MessageBoxButtons.OK);
                 }
-                doc.Save(@"vegetaisXml.xml");
-                rtb_display.Text = final;
-                tb_filePath.Text = System.IO.Directory.GetCurrentDirectory().ToString() + "\\vegetaisXml.xml";
-                FormProjIS.Properties.Settings.Default.XML = tb_filePath.Text;
-
-                MessageBox.Show("XML criado com sucesso", "Sucesso", MessageBoxButtons.OK);
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message, "Erro", MessageBoxButtons.OK);
+                    throw;
+                }
             }
         }
 
@@ -161,90 +73,20 @@ namespace FormProjIS
         {
             openFileDialog1.Filter = "EXCEL Files|*.xls";
             openFileDialog1.InitialDirectory = Application.StartupPath;
-
-            string final = "";
-
+            
             if (openFileDialog1.ShowDialog() == System.Windows.Forms.DialogResult.OK)
             {
-                Excel.Application excelApplication = new Excel.Application();
-                excelApplication.Visible = false;
-                Excel.Workbook excelWorkBook = excelApplication.Workbooks.Open(openFileDialog1.FileName);
-                Excel.Worksheet excelWorkSheet = excelWorkBook.ActiveSheet;
-                Excel.Range excelRange = excelWorkSheet.UsedRange;
-
-                //int numberOfRows = excelRange.Rows.Count;
-                //int numberOfCols = excelRange.Columns.Count;
-
-                XmlDocument doc = new XmlDocument();
-
-                XmlDeclaration dec = doc.CreateXmlDeclaration("1.0", null, null);
-                doc.AppendChild(dec);
-
-                XmlElement restaurantes = doc.CreateElement("restaurantes");
-                doc.AppendChild(restaurantes);
-
-
-                for (int row = 2; row < excelRange.Rows.Count; row++)
+                try
                 {
-                    XmlElement restaurante = doc.CreateElement("restaurante");
-                    restaurante.SetAttribute("nomeRestaurante", excelWorkSheet.Cells[row, 1].Value);
-
-                    XmlElement prato = doc.CreateElement("prato");
-
-                    XmlElement nomeXml = doc.CreateElement("nome");
-                    nomeXml.InnerText = excelWorkSheet.Cells[row, 2].Value;
-
-                    XmlElement kcalXml = doc.CreateElement("kcal");
-                    kcalXml.InnerText = excelWorkSheet.Cells[row, 4].Value;
-
-                    XmlElement quantidadeXml = doc.CreateElement("quantidade");
-                    quantidadeXml.InnerText = excelWorkSheet.Cells[row, 3].Value;
-
-                    prato.AppendChild(nomeXml);
-                    prato.AppendChild(kcalXml);
-                    prato.AppendChild(quantidadeXml);
-                    restaurante.AppendChild(prato);
-
-                    for (int aux = row + 1; aux < excelRange.Rows.Count; aux++)
-                    {
-                        if (excelWorkSheet.Cells[aux, 1].Value == restaurante.GetAttribute("nomeRestaurante"))
-                        {
-                            XmlElement pratoAux = doc.CreateElement("prato");
-
-                            XmlElement nomeXmlAux = doc.CreateElement("nome");
-                            nomeXmlAux.InnerText = excelWorkSheet.Cells[aux, 2].Value;
-
-                            XmlElement kcalXmlAux = doc.CreateElement("kcal");
-                            kcalXmlAux.InnerText = excelWorkSheet.Cells[aux, 4].Value;
-
-                            XmlElement quantidadeXmlAux = doc.CreateElement("quantidade");
-                            quantidadeXmlAux.InnerText = excelWorkSheet.Cells[aux, 3].Value;
-
-                            pratoAux.AppendChild(nomeXmlAux);
-                            pratoAux.AppendChild(kcalXmlAux);
-                            pratoAux.AppendChild(quantidadeXmlAux);
-                            restaurante.AppendChild(pratoAux);
-
-                            row++;
-                        }
-                    }
-
-                    restaurantes.AppendChild(restaurante);
-
-                    doc.Save(@"restaurantesXml.xml");
-                    tb_filePath.Text = System.IO.Directory.GetCurrentDirectory().ToString() + "\\restaurantesXml.xml";
-                    FormProjIS.Properties.Settings.Default.XML = tb_filePath.Text;
-
-                    //para tirar futuro
-                    Restaurante rest = new Restaurante(excelWorkSheet.Cells[row, 1].Value,
-                    excelWorkSheet.Cells[row, 2].Value, excelWorkSheet.Cells[row, 3].Value,
-                    excelWorkSheet.Cells[row, 4].Value);
-                    final += rest.ToString() + "\n";
+                    String filePath = HandlerXmlXsdcs.criarXMLExcel(openFileDialog1.FileName);
+                    tb_filePath.Text = filePath;
+                    MessageBox.Show("XML criado com sucesso", "Sucesso", MessageBoxButtons.OK);
                 }
-                rtb_display.Clear();
-                rtb_display.Text = final;
-
-                MessageBox.Show("XML criado com sucesso", "Sucesso", MessageBoxButtons.OK);
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message, "Erro", MessageBoxButtons.OK);
+                    throw;
+                }
             }
         }
 
@@ -253,13 +95,8 @@ namespace FormProjIS
             bool isValid = true;
             try
             {
-                XmlReaderSettings settings = new XmlReaderSettings();
-                settings.Schemas.Add(null, xsd);
-                settings.ValidationType = ValidationType.Schema;
-
-                XmlReader reader = XmlReader.Create(xml, settings);
-                XmlDocument doc = new XmlDocument();
-                doc.Load(reader);
+                HandlerXmlXsdcs.validarXML(xml, xsd);
+                MessageBox.Show("XML validado com sucesso", "Sucesso", MessageBoxButtons.OK);
             }
             catch (Exception ex)
             {
@@ -269,8 +106,6 @@ namespace FormProjIS
                 {
                     MessageBox.Show(i);
                 }
-
-                //MessageBox.Show(ex.Message, "titulo", MessageBoxButtons.OK);
             }
             finally
             {
@@ -296,6 +131,7 @@ namespace FormProjIS
                             tb_filePath.Text = op1.FileName;
                             xml = op1.FileName;
                             FormProjIS.Properties.Settings.Default.XML = op1.FileName;
+                            Properties.Settings.Default.Save();
                         }
                     }
                 }
@@ -324,6 +160,7 @@ namespace FormProjIS
                             tb_xsd.Text = op1.FileName;
                             xsd = op1.FileName;
                             FormProjIS.Properties.Settings.Default.XSD = op1.FileName;
+                            Properties.Settings.Default.Save();
                         }
                     }
                 }
@@ -332,6 +169,15 @@ namespace FormProjIS
                     MessageBox.Show("Error: Could not read file from disk. Original error: " + ex.Message);
                 }
             }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            tb_filePath.Text = FormProjIS.Properties.Settings.Default.XML;
+            tb_xsd.Text = FormProjIS.Properties.Settings.Default.XSD;
+
+            xml = tb_filePath.Text = FormProjIS.Properties.Settings.Default.XML;
+            xsd = tb_xsd.Text = FormProjIS.Properties.Settings.Default.XSD;
         }
     }
 }

a7bed5e25e8eca846ed87405b0de50bef924d500
KaskasComputer <KaskasComputer@DESKTOP-CGPPL9F>
2016-12-09 10:52:15 +0000

    criacao webservice e os metodos necessarios

---
+++ ProjIS/WebService/IService1.cs
@@ -0,0 +1,275 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace WebService
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IService1" in both code and config file together.
+    [ServiceContract]
+    public interface IService1
+    {
+        // AUTHENTICATION
+        [WebInvoke(Method = "POST", UriTemplate = "/signup?token={token}")]
+        [OperationContract]
+        void SignUp(User user, string token); // admin only
+
+        [WebInvoke(Method = "POST", UriTemplate = "/login?username={username}&password={password}")]
+        [OperationContract]
+        string LogIn(string username, string password);
+
+        [WebInvoke(Method = "POST", UriTemplate = "/logout")]
+        [OperationContract]
+        void LogOut(string token);
+
+        [OperationContract]
+        [WebInvoke(Method = "GET", UriTemplate = "/isadmin?token={token}")]
+        bool IsAdmin(string token);
+
+        [OperationContract]
+        [WebInvoke(Method = "GET", UriTemplate = "/isloggedin?token={token}")]
+        bool IsLoggedIn(string token);
+
+        // ------ PARA FAZER ------
+
+        // ADD 
+        [OperationContract]
+        [WebInvoke(Method = "POST", UriTemplate = "/vegetal?token={token}")]
+        void AddVegetal(Vegetal vegetal, string token); 
+
+        [OperationContract]
+        [WebInvoke(Method = "POST", UriTemplate = "/exercicio?token={token}")]
+        void AddExercicio(Exercicio exercicio, string token); 
+
+        [OperationContract]
+        [WebInvoke(Method = "POST", UriTemplate = "/restaurante?token={token}")]
+        void AddPrato(Restaurante restaurante, string token); 
+
+        // DELETE 
+        [OperationContract(Name = "DeleteVegetalByNome")]
+        [WebInvoke(Method = "DELETE", UriTemplate = "/vegetal/{title}?token={token}")]
+        void DeleteVegetal(string title, string token); 
+
+        [OperationContract(Name = "DeleteExercicioByNome")]
+        [WebInvoke(Method = "DELETE", UriTemplate = "/exercicio/{title}?token={token}")]
+        void DeleteExercicio(string title, string token); 
+
+        // ----- delete refeicao tem de ter restaurante e prato -----
+        [OperationContract(Name = "DeleteRefeicaoByNome")]
+        [WebInvoke(Method = "DELETE", UriTemplate = "/restaurante/{title}?token={token}")]
+        void DeletePrato(string title, string token);
+
+        // GET
+        [OperationContract(Name = "GetBookByTitle")]
+        [WebInvoke(Method = "GET", UriTemplate = "/book/{title}?token={token}")]
+        Vegetal GetCaloriasByVegetal(string title, string token);
+
+        [OperationContract(Name = "GetBookByTitle")]
+        [WebInvoke(Method = "GET", UriTemplate = "/book/{title}?token={token}")]
+        Exercicio GetCaloriasByExercicio(string title, string token);
+
+        [OperationContract(Name = "GetBookByTitle")]
+        [WebInvoke(Method = "GET", UriTemplate = "/book/{title}?token={token}")]
+        Restaurante GetCaloriasByPrato(string title, string token);
+
+        [OperationContract(Name = "GetBooksByCategory")]
+        [WebInvoke(Method = "GET", UriTemplate = "/books/{category}?token={token}")]
+        List<Vegetal> GetVegetaisByCalorias(string category, string token);
+
+        [OperationContract(Name = "GetBooksByCategory")]
+        [WebInvoke(Method = "GET", UriTemplate = "/books/{category}?token={token}")]
+        List<Exercicio> GetExerciciosByCalorias(string category, string token);
+    }
+
+
+    // Use a data contract as illustrated in the sample below to add composite types to service operations.
+    [DataContract]
+    public class User
+    {
+        private string username;
+        private string password;
+        private bool admin;
+
+        public User(string username, string password, bool admin)
+        {
+            this.admin = admin;
+            this.username = username;
+            this.password = password;
+        }
+
+        [DataMember]
+        public bool Admin
+        {
+            get { return admin; }
+            set { admin = value; }
+        }
+
+        [DataMember]
+        public string Username
+        {
+            get { return username; }
+            set { username = value; }
+        }
+
+        [DataMember]
+        public string Password
+        {
+            get { return password; }
+            set { password = value; }
+        }
+    }
+
+    [DataContract]
+    public class Vegetal
+    {
+        private string nome;
+        private string calorias;
+        private string dose;
+        private string estado;
+
+        public Vegetal(string nome, string estado, string calorias, string dose)
+        {
+            this.nome = nome;
+            this.estado = estado;
+            this.calorias = calorias;
+            this.dose = dose;
+        }
+
+        public string Nome
+        {
+            get { return nome; }
+            set { nome = value; }
+        }
+
+        public string Estado
+        {
+            get { return estado; }
+            set { estado = value; }
+        }
+
+        public string Calorias
+        {
+            get { return calorias; }
+            set { calorias = value; }
+        }
+
+        public string Dose
+        {
+            get { return dose; }
+            set { dose = value; }
+        }
+
+        public override string ToString()
+        {
+            return nome + "|" + estado + "|" + calorias + " kcal|" + dose;
+        }
+    }
+
+    [DataContract]
+    public class Restaurante
+    {
+        private string nomeRestaurante;
+        private string nome;
+        private string quantidade;
+        private string calorias;
+
+        public Restaurante(string nomeRestaurante, string nome, string quantidade, string calorias)
+        {
+            this.nomeRestaurante = nomeRestaurante;
+            this.nome = nome;
+            this.quantidade = quantidade;
+            this.calorias = calorias;
+        }
+
+        public string NomeRestaurante
+        {
+            get { return nomeRestaurante; }
+            set { nomeRestaurante = value; }
+        }
+
+        public string Nome
+        {
+            get { return nome; }
+            set { nome = value; }
+        }
+
+        public string Quantidade
+        {
+            get { return quantidade; }
+            set { quantidade = value; }
+        }
+
+        public string Calorias
+        {
+            get { return calorias; }
+            set { calorias = value; }
+        }
+
+        public override string ToString()
+        {
+            return nomeRestaurante + " " + nome + " " + quantidade + " " + calorias;
+        }
+    }
+
+    [DataContract]
+    public class Exercicio
+    {
+        private string nome;
+        private int calorias;
+        private float met;
+
+        public Exercicio(string nome, int calorias, float met)
+        {
+            this.Nome = nome;
+            this.Calorias = calorias;
+            this.Met = met;
+        }
+
+        public string Nome
+        {
+            get
+            {
+                return nome;
+            }
+
+            set
+            {
+                nome = value;
+            }
+        }
+
+        public int Calorias
+        {
+            get
+            {
+                return calorias;
+            }
+
+            set
+            {
+                calorias = value;
+            }
+        }
+
+        public float Met
+        {
+            get
+            {
+                return met;
+            }
+
+            set
+            {
+                met = value;
+            }
+        }
+
+        public override string ToString()
+        {
+            return nome + " " + calorias + " " + met;
+        }
+    }
+}
---
+++ ProjIS/WebService/Service1.svc.cs
@@ -0,0 +1,433 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+using System.Xml;
+
+namespace WebService
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select Service1.svc or Service1.svc.cs at the Solution Explorer and start debugging.
+    public class Service1 : IService1
+    {
+        // chave: username
+        // valor: objeto user
+        private Dictionary<string, User> users;
+
+        // chave: uid
+        // valor: objeto Token
+        private Dictionary<string, Token> tokens;
+
+        private static string FILEPATH;
+
+        private class Token
+        {
+            private string value; // uid
+            private long timeout; // daqui a quanto tempo é que o token deixa de ser valido
+            private User user; // user ao qual o token está assiciado
+
+            public Token(User user) : this(user, 240000) // token válido por 4 minutos 
+            { }
+
+            public Token(User user, long timeout)
+            {
+                this.value = Guid.NewGuid().ToString();
+                this.timeout = Environment.TickCount + timeout;
+                this.user = user;
+            }
+
+            public string Value
+            {
+                get { return value; }
+            }
+
+            public long Timeout
+            {
+                get { return timeout; }
+            }
+
+            public User User
+            {
+                get { return user; }
+            }
+
+            public string Username
+            {
+                get { return user.Username; }
+            }
+
+            public void UpdateTimeout()
+            {
+                UpdateTimeout(240000); // token renovado por 4 minutos
+            }
+
+            public void UpdateTimeout(long timeout)
+            {
+                this.timeout = Environment.TickCount + timeout;
+            }
+
+            public Boolean isTimeoutExpired()
+            {
+                return Environment.TickCount > timeout;
+            }
+        }
+
+        public Service1()
+        {
+            // instacia os dois Dictionaries
+            this.users = new Dictionary<string, User>();
+            this.tokens = new Dictionary<string, Token>();
+
+            // default administrator
+            // cria um primeiro utilizador de administração
+            users.Add("admin", new User("admin", "admin", true));
+
+            // define a filepath do ficheiro bookstore.xml
+            //FILEPATH = Path.Combine(HostingEnvironment.ApplicationPhysicalPath, "App_Data", "bookstore.xml");
+        }
+
+        public void SignUp(User user, string token)
+        {
+            checkAuthentication(token, true);
+
+            // ver se o username ja existe
+            if (users.Keys.Contains(user.Username))
+            {
+                throw new ArgumentException("ERROR: username already exists: " + user.Username);
+            }
+
+            // acrescenta o user recebido ao dicionario utilizado 
+            // o username como chave
+            users.Add(user.Username, user);
+        }
+
+        public string LogIn(string username, string password)
+        {
+            cleanUpTokens();
+
+            if (!String.IsNullOrEmpty(username) && !String.IsNullOrEmpty(password) && password.Equals(users[username].Password))
+            {
+                Token tokenObject = new Token(users[username]);
+                tokens.Add(tokenObject.Value, tokenObject);
+                return tokenObject.Value;
+            }
+            else
+            {
+                throw new ArgumentException("ERROR: invalid username/password combination.");
+            }
+        }
+
+        public void LogOut(string token)
+        {
+            tokens.Remove(token);
+            cleanUpTokens();
+        }
+
+        public bool IsAdmin(string token)
+        {
+            return tokens[token].User.Admin;
+        }
+
+        public bool IsLoggedIn(string token)
+        {
+            bool res = true;
+
+            try
+            {
+                checkAuthentication(token, false);
+            }
+            catch (ArgumentException)
+            {
+                res = false;
+            }
+            return res;
+        }
+
+        private void cleanUpTokens()
+        {
+            // percorre os Tokens que estão no dictionary e remove-os se já tiverem expirados
+            foreach (Token tokenObject in tokens.Values)
+            {
+                if (tokenObject.isTimeoutExpired())
+                {
+                    tokens.Remove(tokenObject.Username);
+                }
+            }
+        }
+
+        private Token checkAuthentication(string token, bool mustBeAdmin)
+        {
+            Token tokenObject;
+
+            if (String.IsNullOrEmpty(token))
+            {
+                throw new ArgumentException("ERROR: invalid token value.");
+            }
+
+            try
+            {
+                tokenObject = tokens[token];
+            }
+            catch (KeyNotFoundException)
+            {
+                throw new ArgumentException("ERROR: user is not logged in (expired session?).");
+            }
+
+            if (tokenObject.isTimeoutExpired())
+            {
+                tokens.Remove(tokenObject.Username);
+                throw new ArgumentException("ERROR: the session has expired. Please log in again.");
+            }
+
+            // se chegou aqui é porque: 
+            // - o uid passado nao é nulo
+            // - o uid existe no dicionario de tokens, e foi possivel obter o objeto Token
+            // - o Token tem um timeout que ainda nao expirou
+            // caso contrario, teria sido lançada uma ArgumentException com uma explicacao do erro
+
+            // aqui vai-se verificar se, caso o parametro mustBeAdmin esteja a true, o user ao qual foi dado o token é admin
+            if (mustBeAdmin && !tokens[token].User.Admin)
+            {
+                throw new ArgumentException("ERROR: only admins are allowed to perform this operation.");
+            }
+
+            // quando se chega aqui é porque o token é valido, e renova-lo em 4 min
+            tokenObject.UpdateTimeout();
+            return tokenObject;
+        }
+
+        // ------ PARA FAZER ------
+
+        // ADD
+        public void AddVegetal(Vegetal vegatal, string token)
+        {
+            checkAuthentication(token, true);
+            XmlDocument doc = new XmlDocument();
+            doc.Load(FILEPATH);
+            XmlNode bookstoreNode = doc.SelectSingleNode("/bookstore");
+            XmlElement bookNode = doc.CreateElement("book");
+            bookNode.SetAttribute("CATEGORY", book.Category);
+            XmlElement titleNode = doc.CreateElement("title");
+            titleNode.InnerText = book.Title;
+            bookNode.AppendChild(titleNode);
+            XmlElement authorNode = doc.CreateElement("author");
+            authorNode.InnerText = book.Author;
+            bookNode.AppendChild(authorNode);
+            XmlElement yearNode = doc.CreateElement("year");
+            yearNode.InnerText = book.Year.ToString();
+            bookNode.AppendChild(yearNode);
+            XmlElement priceNode = doc.CreateElement("price");
+            priceNode.InnerText = Convert.ToString(book.Price, NumberFormatInfo.InvariantInfo);
+            bookNode.AppendChild(priceNode);
+            bookstoreNode.AppendChild(bookNode);
+            doc.Save(FILEPATH);
+        }
+
+        public void AddExercicio(Exercicio exercicio, string token)
+        {
+            checkAuthentication(token, true);
+            XmlDocument doc = new XmlDocument();
+            doc.Load(FILEPATH);
+            XmlNode bookstoreNode = doc.SelectSingleNode("/bookstore");
+            XmlElement bookNode = doc.CreateElement("book");
+            bookNode.SetAttribute("CATEGORY", book.Category);
+            XmlElement titleNode = doc.CreateElement("title");
+            titleNode.InnerText = book.Title;
+            bookNode.AppendChild(titleNode);
+            XmlElement authorNode = doc.CreateElement("author");
+            authorNode.InnerText = book.Author;
+            bookNode.AppendChild(authorNode);
+            XmlElement yearNode = doc.CreateElement("year");
+            yearNode.InnerText = book.Year.ToString();
+            bookNode.AppendChild(yearNode);
+            XmlElement priceNode = doc.CreateElement("price");
+            priceNode.InnerText = Convert.ToString(book.Price, NumberFormatInfo.InvariantInfo);
+            bookNode.AppendChild(priceNode);
+            bookstoreNode.AppendChild(bookNode);
+            doc.Save(FILEPATH);
+        }
+
+        public void AddPrato(Restaurante restaurante, string token)
+        {
+            checkAuthentication(token, true);
+            XmlDocument doc = new XmlDocument();
+            doc.Load(FILEPATH);
+            XmlNode bookstoreNode = doc.SelectSingleNode("/bookstore");
+            XmlElement bookNode = doc.CreateElement("book");
+            bookNode.SetAttribute("CATEGORY", book.Category);
+            XmlElement titleNode = doc.CreateElement("title");
+            titleNode.InnerText = book.Title;
+            bookNode.AppendChild(titleNode);
+            XmlElement authorNode = doc.CreateElement("author");
+            authorNode.InnerText = book.Author;
+            bookNode.AppendChild(authorNode);
+            XmlElement yearNode = doc.CreateElement("year");
+            yearNode.InnerText = book.Year.ToString();
+            bookNode.AppendChild(yearNode);
+            XmlElement priceNode = doc.CreateElement("price");
+            priceNode.InnerText = Convert.ToString(book.Price, NumberFormatInfo.InvariantInfo);
+            bookNode.AppendChild(priceNode);
+            bookstoreNode.AppendChild(bookNode);
+            doc.Save(FILEPATH);
+        }
+
+        // DELETE
+        public void DeleteVegetal(string title, string token)
+        {
+            checkAuthentication(token, true);
+            XmlDocument doc = new XmlDocument();
+            doc.Load(FILEPATH);
+            XmlNode bookstoreNode = doc.SelectSingleNode("/bookstore");
+            XmlNode bookNode = doc.SelectSingleNode("/bookstore/book[title='" + title + "']");
+
+            if (bookNode != null)
+            {
+                bookstoreNode.RemoveChild(bookNode);
+                doc.Save(FILEPATH);
+            }
+        }
+
+        public void DeleteExercicio(string title, string token)
+        {
+            checkAuthentication(token, true);
+            XmlDocument doc = new XmlDocument();
+            doc.Load(FILEPATH);
+            XmlNode bookstoreNode = doc.SelectSingleNode("/bookstore");
+            XmlNode bookNode = doc.SelectSingleNode("/bookstore/book[title='" + title + "']");
+
+            if (bookNode != null)
+            {
+                bookstoreNode.RemoveChild(bookNode);
+                doc.Save(FILEPATH);
+            }
+            checkAuthentication(token, true);
+        }
+
+        public void DeletePrato(string title, string token) 
+        {
+            checkAuthentication(token, true);
+            XmlDocument doc = new XmlDocument();
+            doc.Load(FILEPATH);
+            XmlNode bookstoreNode = doc.SelectSingleNode("/bookstore");
+            XmlNode bookNode = doc.SelectSingleNode("/bookstore/book[title='" + title + "']");
+
+            if (bookNode != null)
+            {
+                bookstoreNode.RemoveChild(bookNode);
+                doc.Save(FILEPATH);
+            }
+        }
+
+        // GET
+        public Vegetal GetCaloriasByVegetal(string title, string token)
+        {
+            checkAuthentication(token, false);
+            XmlDocument doc = new XmlDocument();
+            doc.Load(FILEPATH);
+            XmlNode titleNode = doc.SelectSingleNode("/bookstore/book[title='" + title + "']/title");
+
+            if (titleNode == null)
+            {
+                return null;
+            }
+
+            XmlNode authorNode = doc.SelectSingleNode("/bookstore/book[title='" + title + "']/author");
+            XmlNode yearNode = doc.SelectSingleNode("/bookstore/book[title='" + title + "']/year");
+            XmlNode priceNode = doc.SelectSingleNode("/bookstore/book[title='" + title + "']/price");
+            XmlNode categoryNode = doc.SelectSingleNode("/bookstore/book[title='" + title + "']/@CATEGORY");
+            Book book = new Book(titleNode.InnerText, authorNode.InnerText, Convert.ToInt32(yearNode.InnerText), Convert.ToDouble(priceNode.InnerText, NumberFormatInfo.InvariantInfo), categoryNode.InnerText);
+
+            return book;
+        }
+
+        public Exercicio GetCaloriasByExercicio(string title, string token)
+        {
+            checkAuthentication(token, false);
+            XmlDocument doc = new XmlDocument();
+            doc.Load(FILEPATH);
+            XmlNode titleNode = doc.SelectSingleNode("/bookstore/book[title='" + title + "']/title");
+
+            if (titleNode == null)
+            {
+                return null;
+            }
+
+            XmlNode authorNode = doc.SelectSingleNode("/bookstore/book[title='" + title + "']/author");
+            XmlNode yearNode = doc.SelectSingleNode("/bookstore/book[title='" + title + "']/year");
+            XmlNode priceNode = doc.SelectSingleNode("/bookstore/book[title='" + title + "']/price");
+            XmlNode categoryNode = doc.SelectSingleNode("/bookstore/book[title='" + title + "']/@CATEGORY");
+            Book book = new Book(titleNode.InnerText, authorNode.InnerText, Convert.ToInt32(yearNode.InnerText), Convert.ToDouble(priceNode.InnerText, NumberFormatInfo.InvariantInfo), categoryNode.InnerText);
+
+            return book;
+        }
+
+        public Restaurante GetCaloriasByPrato(string title, string token)
+        {
+            checkAuthentication(token, false);
+            XmlDocument doc = new XmlDocument();
+            doc.Load(FILEPATH);
+            XmlNode titleNode = doc.SelectSingleNode("/bookstore/book[title='" + title + "']/title");
+
+            if (titleNode == null)
+            {
+                return null;
+            }
+
+            XmlNode authorNode = doc.SelectSingleNode("/bookstore/book[title='" + title + "']/author");
+            XmlNode yearNode = doc.SelectSingleNode("/bookstore/book[title='" + title + "']/year");
+            XmlNode priceNode = doc.SelectSingleNode("/bookstore/book[title='" + title + "']/price");
+            XmlNode categoryNode = doc.SelectSingleNode("/bookstore/book[title='" + title + "']/@CATEGORY");
+            Book book = new Book(titleNode.InnerText, authorNode.InnerText, Convert.ToInt32(yearNode.InnerText), Convert.ToDouble(priceNode.InnerText, NumberFormatInfo.InvariantInfo), categoryNode.InnerText);
+
+            return book;
+        }
+
+        public List<Vegetal> GetVegetaisByCalorias(string category, string token)
+        {
+            checkAuthentication(token, false);
+            XmlDocument doc = new XmlDocument();
+            doc.Load(FILEPATH);
+            XmlNodeList bookNodes = doc.SelectNodes("/bookstore/book[@CATEGORY='" + category + "']");
+            List<Book> books = new List<Book>();
+
+            foreach (XmlNode bookNode in bookNodes)
+            {
+                XmlNode titleNode = bookNode.SelectSingleNode("title");
+                XmlNode authorNode = bookNode.SelectSingleNode("author");
+                XmlNode yearNode = bookNode.SelectSingleNode("year");
+                XmlNode priceNode = bookNode.SelectSingleNode("price");
+                XmlAttribute categoryNode = bookNode.Attributes["CATEGORY"];
+                Book book = new Book(titleNode.InnerText, authorNode.InnerText, Convert.ToInt32(yearNode.InnerText), Convert.ToDouble(priceNode.InnerText, NumberFormatInfo.InvariantInfo), categoryNode.Value);
+                books.Add(book);
+            }
+
+            return books;
+        }
+
+        public List<Exercicio> GetExerciciosByCalorias(string category, string token)
+        {
+            checkAuthentication(token, false);
+            XmlDocument doc = new XmlDocument();
+            doc.Load(FILEPATH);
+            XmlNodeList bookNodes = doc.SelectNodes("/bookstore/book[@CATEGORY='" + category + "']");
+            List<Book> books = new List<Book>();
+
+            foreach (XmlNode bookNode in bookNodes)
+            {
+                XmlNode titleNode = bookNode.SelectSingleNode("title");
+                XmlNode authorNode = bookNode.SelectSingleNode("author");
+                XmlNode yearNode = bookNode.SelectSingleNode("year");
+                XmlNode priceNode = bookNode.SelectSingleNode("price");
+                XmlAttribute categoryNode = bookNode.Attributes["CATEGORY"];
+                Book book = new Book(titleNode.InnerText, authorNode.InnerText, Convert.ToInt32(yearNode.InnerText), Convert.ToDouble(priceNode.InnerText, NumberFormatInfo.InvariantInfo), categoryNode.Value);
+                books.Add(book);
+            }
+
+            return books;
+        }
+    }
+}
